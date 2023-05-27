e4d22c08ca1f5f55277f85e275e272e4042733d7
Masaori Hirono <masaori.hirono@gmail.com>
2023-05-27 16:39:11 -0500

    Fixed the prompt and error handling

--- main.py
+++ main.py
@@ -14,9 +14,6 @@ from langchain.agents.conversational_chat.output_parser import ConvoOutputParser
 from langchain.memory import ConversationBufferMemory
 import os
 
-from gpt4_output_parser import Gpt4OutputParser
-
-
 os.environ["OPENAI_API_KEY"] = open("./openapi_key.txt", "r").read().strip()
 
 # output_parser = Gpt4OutputParser()
@@ -29,17 +26,22 @@ def main():
     llm = ChatOpenAI(model_name='gpt-4')
 
     tools: list[BaseTool] = [
-        ShellTool(),
-        ReadFileTool(),
+        ShellTool("""Use this to run shell command.
+            Please move to the project directory before running the command like this:
+            cd <absolute/path/to/project> && <command>
+        """),
+        ReadFileTool("""Use this to read file from specific path you want to read.
+            Please make sure that the file exists with using ListDirectoryTool.
+        """),
         WriteFileTool(
             description="""Use this to write file out to specific path you want to store.
         
-        Action Plan Format:
-        {{
-            "file_path": string \\ The absolute file path to write to
-            "text": string \\ The content to write to the file
-            "append": bool \\ Whether to append to an existing file
-        }}
+            Input Format:
+            {{
+                "file_path": string \\ The absolute file path to write to
+                "text": string \\ The content to write to the file
+                "append": bool \\ Whether to append to an existing file
+            }}
         """),
         ListDirectoryTool(),
         # TODO: Make custom tool using gh command
@@ -48,12 +50,43 @@ def main():
     tool_descriptions = [f"{tool.name}: {tool.description}" for tool in tools]
     tool_names = [tool.name for tool in tools]
 
-    action_play_history = []
+    action_plan_history = []
+    last_output = None
+    action_times = 0
     while True:
-        action_plan_prompt = f"""
+        print('====')
+        print(f"==== Start Action ==== {action_times}")
+        action_times += 1
+
+        prompt = f"""
             What I want you to do:
-            Please summary "what the following Typescript project aim" and write it out to README.md file to explain the project properly in Markdown notation.
-            Project path: /Users/masaori/git/masaori/auto-test-writer-example-typescript
+                1. Write a Test
+                    - Please make a new branch with appropriate name.
+                    - Please write a test file for the follwoing .ts file.
+                        - Target ts File /Users/masaori/git/masaori/auto-test-writer-example-typescript/src/domain/usecases/CreateUserUsecase.ts
+                    - Please output the test file at the same directory as the actual ts file.
+                    - If you need, please read associated files seeing import statements.
+                2. Check your Test file
+                    - Please check if the transpiling succeeds.
+                    - Please check if your tests pass correctly.
+                    - If it fails, please fix your test file.
+                3. Commit your Test file and Make Pull Request
+                    - After you confirm that your test file is correct, please commit your test file.
+                    - Please make a pull request to the main branch.
+
+            Tips:
+                - If you want to check if the typescript code transpiles properly, please run the following command:
+                    - `npx tsc --noEmit`
+                - If you want to check if the test code succeeds, please run the following command:
+                    - `npx jest <path/to/test/file>`
+                - If you want to check current git status, you can use the following shell command:
+                    - `git status`
+                - If you want to make your own branch, please run the following command:
+                    - `git checkout -b <your branch name>`
+                - If you want to commit your changes, please run the following command:
+                    - `git add . && git commit -m "<appropriate commit message>" && git push -u origin <your branch name>`
+                - If you want to make Pull Request, you can use the following shell command:
+                    - `gh pr create --base main --title "<Your PR title>" --body "<Your PR body>"`
 
             Tools you can use:
             {tool_descriptions}
@@ -61,7 +94,7 @@ def main():
             Output:
             When responding to me, please output a response in one of two formats:
 
-            **Option 1:**
+            **Option #1:**
             Use this if you want the human to use a tool.
             Markdown code snippet formatted in the following schema:
 
@@ -69,6 +102,7 @@ def main():
                 "thought": string \\ The thought that led to this action
                 "action": string \\ The action to take. Must be one of {tool_names}
                 "action_input": string \\ The input to the action
+                "save_to_history": bool \\ Whether to save this action and the output of tools to the history
             }}
 
             **Option #2:**
@@ -79,21 +113,41 @@ def main():
                 "action_input": string \\ You should put what you want to return to use here
             }}
 
-            History of your past Action Plans:
-            {action_play_history}
-        """
-        # print(action_plan_prompt)
-        action_plan_output = llm.predict(action_plan_prompt)
-        # print(action_plan_output)
-        action_plan = json.loads(action_plan_output)
-        print('==== Action Plan ====')
-        print(json.dumps(action_plan, indent=2))
+            Last Output:
+            {last_output}
 
-        # If the action plan is a final answer, then return it
-        if action_plan['action'] == 'Final Answer':
-            print('==== Final Answer ====')
-            print(action_plan['action_input'])
-            break
+            History of your past Action Plans:
+            {action_plan_history}
+        """
+
+        action_plan_output = llm.predict(prompt)
+
+        try:
+            action_plan = json.loads(action_plan_output)
+            if action_plan['action'] == 'Final Answer':
+                print('==== Final Answer ====')
+                print(action_plan['action_input'])
+                break
+
+            print('==== Action Plan ====')
+            print(f"Thought: {action_plan['thought']}")
+            print(f"Action: {action_plan['action']}")
+            print(f"Action Input: {action_plan['action_input']}")
+            print(f"Save to History: {action_plan['save_to_history']}")
+        except json.decoder.JSONDecodeError as e:
+            print(f"==== JSON Decode Error ==== {action_plan_output}")
+            last_output = {
+                "error_message": "Failed to decode your response as JSON. Please try again.",
+            }
+            continue
+        except KeyError as e:
+            print(f"==== Key Error ==== {action_plan_output}")
+            last_output = {
+                "error_message": f"Failed to find key {e.args} in your response. Please try again.",
+            }
+            continue
+        except Exception as e:
+            raise e
 
         # Find the tool that matches the action plan from tools
         target_tool = None
@@ -102,8 +156,12 @@ def main():
                 target_tool = tool
                 break
         if target_tool is None:
-            raise Exception(
-                f"Could not find tool with name {action_plan['action']}")
+            print(f"==== Tool Not Found ==== {action_plan_output}")
+            last_output = {
+                "error_message": f"Failed to find tool {action_plan['action']} in your response. Please try again.",
+            }
+            continue
+
         print(f"==== Using tool: {target_tool.name} ====")
 
         action_plan_input: object
@@ -124,7 +182,7 @@ def main():
         print('==== Tool Output ====')
         print(tool_output)
 
-        action_play_history.append({
+        action_plan_history.append({
             "thought": action_plan['thought'],
             "action": action_plan['action'],
             "action_input": action_plan['action_input'],
